9a40d94adef61838226697dd4d3fa3f48f017eb8
ildimas <dimailyushen@gmail.com>
2023-08-11 16:12:02 +0300

    photo essentials

--- project/main/admin.py
+++ project/main/admin.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 from .models import *
 
 class ShopAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "time_create")
+    fields = ("title", "time_create", "description", "image", "admin_photo")
+    list_display = ("id", "title", "time_create", "admin_photo")
     search_fields = ("title", "id", "time_create")
+    readonly_fields = ("time_create", "admin_photo")
+    actions = ['delete_selected']
     
+    def delete_selected(self, request, queryset):
+        for shop in queryset:
+            if shop.image:
+                shop.image.delete()
+            shop.delete()
+        self.message_user(request, f"Deleted {queryset.count()} shops and associated pictures.")
+
+    delete_selected.short_description = "Удаалить выбранные магазины"
+     
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "title")
     search_fields = ("title", "id")
--- project/main/models.py
+++ project/main/models.py
@@ -1,14 +1,25 @@
 from django.db import models
-
+from django.utils.safestring import mark_safe
 class Shop(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
     description = models.TextField(blank=True, verbose_name="Описание")
-    imageUrl = models.TextField(blank=True)
+    image = models.ImageField(upload_to="shop_pictures/", verbose_name="Фотография")
+    
+    def admin_photo(self):
+        return mark_safe("<img src='{}' width='150' />".format(self.image.url))
+    admin_photo.short_description = "Фотография"
+    admin_photo.allow_tags = True
+    
+    def delete(self, *args, **kwargs):
+        if self.image:
+            self.image.delete()
+        super().delete(*args, **kwargs)
     
     def __str__(self):
         return self.title
+    
     class Meta():
         verbose_name = "Магазин"
         verbose_name_plural = "Магазины"
